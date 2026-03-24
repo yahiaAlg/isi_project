981714566527814423381f0838774512de4de91a
yahiaAlg <yahialinus21alg@gmail.com>
2026-03-24 10:43:06 +0100

    `Update ALLOWED_HOSTS in settings.py to include '*'`

---
+++ build.sh
@@ -0,0 +1,13 @@
+pip install -r requirements.txt
+
+find . -path "./.venv" -prune -o -path "*/migrations/0*.py" -print | xargs rm -f
+
+python manage.py makemigrations core clients accounts etudes formations resources financial
+
+rm -f db.sqlite3
+
+python manage.py migrate
+
+python manage.py collectstatic --noinput -v 0
+
+python manage.py seed_db
--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config(
 
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*,127.0.0.1,localhost", cast=Csv())
 
 
 # ── Application definition ────────────────────────────────────────────────── #
